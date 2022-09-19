813a5d292e881fefc00a9e92568f4bae573cf57e
Milad Mohammadi <phy.milad@gmail.com>
2022-09-20 00:03:09 +0430

    Mobile home page initial version finished

--- src/components/footer/MainFooter.js
+++ src/components/footer/MainFooter.js
@@ -1,5 +1,11 @@
 import React from "react"
+import styles from "./MainFooter.module.css"
 
 export default function MainFooter() {
-  return <div>MainFooter</div>
+  return (
+    <div className={styles.container}>
+      <p>Phone-Number: +989357064101</p>
+      <p>Email-Address: phy.milad@gmail.com</p>
+    </div>
+  )
 }
