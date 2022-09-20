7c8887fa9d484b7cbf0eb4ba65253d595a3b8c5a
Milad Mohammadi <phy.milad@gmail.com>
2022-09-20 21:00:44 +0430

    Header and footer is removed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./components/pages/Home"
 import About from "./components/pages/About"
-import MainHeader from "./components/header/MainHeader"
 import "./styles/App.css"
-import MainFooter from "./components/footer/MainFooter"
 
 function App() {
   return (
     <div className="app-container">
-      <MainHeader />
-      <div className="routes-container">
-        <Routes>
-          <Route path="/" element={<Home />} exact />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-      <MainFooter />
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   )
 }
--- src/components/header/MainHeader.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import styles from "./MainHeader.module.css"
-
-export default function MainHeader() {
-  return <div className={styles.container}>This is the header</div>
-}
