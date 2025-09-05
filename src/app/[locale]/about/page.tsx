d6252e7c2f7552083ae71cfdb86b6a3348788e41
phymilad <phy.milad@gmail.com>
2025-09-05 16:04:11 +0330

    feat: handle first version of the about_me page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,7 +5,13 @@ export default function About({ params }: { params: { locale: Locale } }) {
   return (
     <article className="prose">
       <h1>{t('about_title', locale)}</h1>
-      <p>{t('about_body', locale)}</p>
+      <div className='flex flex-col gap-4'>
+        <p>{t('about_me_first_paragraph', locale)}</p>
+        <p>{t('about_me_second_paragraph', locale)}</p>
+        <p>{t('about_me_third_paragraph', locale)}</p>
+        <p>{t('about_me_forth_paragraph', locale)}</p>
+        <p>{t('about_me_fifth_paragraph', locale)}</p>
+      </div>
     </article>
   );
 }
